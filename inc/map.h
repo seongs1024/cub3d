a97cc5acf31e46de166fc37a54ff0fb203275263
Seongsu Park <seongspa@c2r4s1.42seoul.kr>
2023-05-02 15:32:12 +0900

    norm

--- inc/map.h
+++ inc/map.h
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongspa <seongspa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/02 15:24:55 by seongspa          #+#    #+#             */
+/*   Updated: 2023/05/02 15:28:38 by seongspa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MAP_H
 # define MAP_H
 
 typedef struct s_map
 {
-    char    *north_path;
-    char    *south_path;
-    char    *west_path;
-    char    *east_path;
-    int     f_colors[3];
-    int     c_colors[3];
+	char	*north_path;
+	char	*south_path;
+	char	*west_path;
+	char	*east_path;
+	int		f_colors[3];
+	int		c_colors[3];
 
-    int     map_width;
-    int     map_height;
-    char    **map;
+	int		map_width;
+	int		map_height;
+	char	**map;
 
-    int     player_pos[2];
-    char    player_dir;
-}   t_map;
+	int		player_pos[2];
+	char	player_dir;
+}	t_map;
 
 #endif
